53e2699e7c1e5a2b9574c9afc57f8e30f7a09566
Илья Мельниченко <melil@ya.ru>
2017-10-30 00:28:29 +0300

    img details

--- Lab1/MainPage.xaml.cs
+++ Lab1/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,14 @@ namespace Lab1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        double counter = 0;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+
         }
 
         private void ell_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
@@ -32,31 +38,38 @@ namespace Lab1
             trans.TranslateX += e.Delta.Translation.X;
             trans.TranslateY += e.Delta.Translation.Y;
 
-            if (trans.TranslateX > 500)
+            if (trans.TranslateX >= 250 && trans.TranslateY <= 350)
             {
-                trans.TranslateX = 500; 
-            } else if (trans.TranslateX < 0)
-            {
-                trans.TranslateX = 0;
-            } 
-
-
-            if (trans.TranslateY > 500)
-            {
-                trans.TranslateY = 500;
-            } else if (trans.TranslateY < 0)
-            {
-                trans.TranslateY = 0;
+                counter++;
             }
+            
+    
+            
 
             string transX = Convert.ToString(trans.TranslateX);
             string transY = Convert.ToString(trans.TranslateY);
-            string coord = "x = " + transX + " " + "y = " + transY;
+            
+            string coord = "x = " + transX + " " + "y = " + transY + "counter: " + counter;
             textblock_lbl.Text = coord;
 
             
         }
-        
-       
+
+        private void bird_1(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            birdOneTransform.TranslateX += e.Delta.Translation.X;
+            birdOneTransform.TranslateY += e.Delta.Translation.Y;
+
+
+
+            if(birdOneTransform.TranslateX >= 350)
+            {
+
+            }
+
+
+
+
+        }
     }
 }
